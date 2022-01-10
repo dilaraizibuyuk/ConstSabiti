0731a39f3a6510edd5864ae48055b00b944e570a
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-10 21:21:08 +0300

    Proje dosyası ekle.

---
+++ ConstSabiti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstSabiti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pisayisi = 3.14;
+            int yaricap = 5;
+            double cevre = 2 * yaricap * pisayisi;
+            Console.WriteLine("Dairenin Çevresi:"+cevre);
+            Console.ReadLine();
+        }
+    }
+}
